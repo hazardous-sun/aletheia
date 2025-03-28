5d14576edc103a202a50182af8e22af5e52d3ce5
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:35:53 -0300

    added a script that the fact-check-server container will use to wait until the database is ready

---
+++ server-api/deployments/fact-check-server/wait-for-db.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# wait-for-db.sh
+
+set -e
+
+host="$1"
+shift
+port="$1"
+shift
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
