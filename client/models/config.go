cbf72960e2cc962554622602c4eb2ce9fec3b14b
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:38:11 -0300

    removed 'url' flag from Config model since it should always be required

--- client/models/config.go
+++ client/models/config.go
@@ -1,12 +1,11 @@
 package models
 
 type Config struct {
-	url     bool
 	image   bool
 	video   bool
 	context bool
 }
 
-func NewConfig(url, image, video, context bool) *Config {
-	return &Config{url, image, video, context}
+func NewConfig(image, video, context bool) *Config {
+	return &Config{image, video, context}
 }
