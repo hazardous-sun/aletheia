d5fe8a36bc905c2675bc442c9b5065beb1fe5530
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:39:34 -0300

    turns out fyne initialize a LOT of environment variables, so it just does not makes sense to panic when finding an unrecognized env

--- client/src/models/config.go
+++ client/src/models/config.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	client_errors "fact-ckert-client/src/errors"
-	"fmt"
 	"os"
 )
 
@@ -21,18 +19,17 @@ func CollectConfigValues(config Config) []string {
 
 	for _, v := range os.Environ() {
 		switch v {
-		case "CONTEXT":
-			config.Context = os.Getenv("CONTEXT") == "1"
+		case "--CONTEXT":
+			config.Context = os.Getenv(v) == "1"
 			result = append(result, v)
-		case "IMAGE":
-			config.Image = os.Getenv("IMAGE") == "1"
+		case "--IMAGE":
+			config.Image = os.Getenv(v) == "1"
 			result = append(result, v)
-		case "VIDEO":
-			config.Video = os.Getenv("VIDEO") == "1"
+		case "--VIDEO":
+			config.Video = os.Getenv(v) == "1"
 			result = append(result, v)
 		default:
-			client_errors.Log(fmt.Sprintf("%s -> %s", client_errors.InvalidEnvVariable, v), client_errors.ErrorLevel)
-			panic(client_errors.InvalidEnvVariable)
+			continue
 		}
 	}
 
