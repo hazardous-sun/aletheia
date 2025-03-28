9fdfe1c7b61015a93e9b85158991f53a0ef66316
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:39:35 -0300

    added '-e' to printUsage calls to 'echo' to activate the interpretation of backslash escapes

--- fact-checker/run.sh
+++ fact-checker/run.sh
@@ -3,27 +3,17 @@
 printUsage() {
   echo "Usage: run.sh [OPTIONS]"
   echo "Pod:"
-  echo "  -r \t Resets the project images"
-  echo "  -d \t Deletes the previously stored data"
-  echo "Postgres database:"
-  echo "  --DB_HOST= \t Value used to identify the container where the database is running"
-  echo "  --DB_PORT= \t Value used to identify the port where the database is running"
-  echo "  --DB_NAME= \t Value passed to POSTGRES_DB during the database initialization"
-  echo "  --DB_PASSWORD= \t Value passed to POSTGRES_PASSWORD during the database initialization"
-  echo "  --DB_USER= \t Value passed to POSTGRES_USER during the database initialization"
+  echo -e "  -r \t \t \t Resets the project images"
+  echo -e "  -d \t \t \t Deletes the previously stored data"
+  echo -e "Postgres database:"
+  echo -e "  --DB_HOST= \t \t Value used to identify the container where the database is running"
+  echo -e "  --DB_PORT= \t \t Value used to identify the port where the database is running"
+  echo -e "  --DB_NAME= \t \t Value passed to POSTGRES_DB during the database initialization"
+  echo -e "  --DB_PASSWORD= \t Value passed to POSTGRES_PASSWORD during the database initialization"
+  echo -e "  --DB_USER= \t \t Value passed to POSTGRES_USER during the database initialization"
   echo "Miscelaneous:"
-  echo "  -h --HELP \t Shows the script usage"
+  echo -e "  -h --HELP \t \t Shows the script usage"
 }
-
-# Cleanup previously created containers of this project
-podman-compose down
-
-# Setting environment variables
-export DB_HOST="news-db"
-export DB_PORT="5432"
-export DB_USER="postgres"
-export DB_PASSWORD="1234"
-export DB_NAME="postgres"
 
 # Parse command-line options
 while getopts ":dhr" opt; do
@@ -42,6 +32,7 @@ while getopts ":dhr" opt; do
       ;;
     \?) # handles invalid options
       echo "Invalid option: -$OPTARG" >&2
+      printUsage
       exit 1
       ;;
   esac
@@ -81,12 +72,25 @@ for arg in "$@"; do
     --HELP)
       printUsage
       exit 2
+      ;;
     *)
       # Handle unknown arguments
       echo "Unknown argument: $arg" >&2
+      printUsage
+      exit 1
       ;;
   esac
 done
+
+# Cleanup previously created containers of this project
+podman-compose down
+
+# Setting environment variables
+export DB_HOST="news-db"
+export DB_PORT="5432"
+export DB_USER="postgres"
+export DB_PASSWORD="1234"
+export DB_NAME="postgres"
 
 # Run service
 podman-compose up -d
