39e9a67d83838bb9c94e3e12638c0419f2dc6332
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:39:35 -0300

    update

--- ai-analyzer/run.sh
+++ ai-analyzer/run.sh
@@ -1,32 +1,11 @@
 #!/usr/bin/bash
 
 installOllama() {
-  if command -v ollama &> /dev/null; then
-    echo "ollama is already installed"
-  else
-    echo "ollama is not installed, installing most recent version..."
-
-    if curl -fsSL https://ollama.com/install.sh | sh > install.log 2>&1; then
-      echo "ollama installed successfully"
-    else
-      echo "ollama installation failed!"
-      exit 1
-    fi
-
-    echo "ollama successfully installed!"
-  fi
+  curl -fsSL https://ollama.com/install.sh | sh
 }
 
 installAIModel() {
-  if [[ $(ollama list | grep deepseek) != 0 ]]; then
-    echo "Deepseek LLM not installed. Downloading Deepseek 1.5b model..."
-
-    if [[ $(ollama pull deepseek-r1:1.5b) != 0 ]]; then
-      echo "An error occurred while downloading the LLM model"
-    fi
-
-    echo "Successfully installed the LLM!"
-  fi
+  ollama pull deepseek-r1:1.5b
 }
 
 # Install Ollama if needed
