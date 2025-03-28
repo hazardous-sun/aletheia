795e675559e7672ac75dfcbc849cfde783efd470
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:29:55 -0300

    moved changes from database_repository.go that belonged to language_repository.go

--- server-api/repositories/database_repository.go
+++ server-api/repositories/database_repository.go
@@ -7,66 +7,13 @@ import (
 	"errors"
 )
 
-type DatabaseRepository struct {
+type databaseRepository struct {
     connection *sql.DB
 }
 
-func NewDatabaseRepository(connection *sql.DB) DatabaseRepository {
-	return DatabaseRepository{
+func NewDatabaseRepository(connection *sql.DB) databaseRepository {
+	return databaseRepository{
 		connection: connection,
 	}
 }
 
-// Create ----------------------------------------------------------------------------------
-
-func (dr *DatabaseRepository) CreateLanguage(newsOutlet models.NewsOutlet) (int, error) {
-	_, err := dr.connection.Prepare("INSERT INTO languages (name) VALUES ($1) RETURNING id")
-	
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return -1, errors.New(custom_errors.LanguageNotFound)
-		}
-		// TODO implement missing handler for the scenario where the language already exists
-		return -1, err
-	}
-	
-	return -1, nil
-}
-
-// Read ------------------------------------------------------------------------------------
-
-func (dr *DatabaseRepository) GetLanguages() ([]models.Language, error) {
-	query := "SELECT * FROM languages"
-	rows, err := dr.connection.Query(query)
-
-	if err != nil {
-		custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
-		return []models.Language{}, errors.New(custom_errors.LanguageTableMissing)
-	}
-
-	var languageList []models.Language
-	var languageObj models.Language
-
-	for rows.Next() {
-		err = rows.Scan(
-			&languageObj.Id,
-			&languageObj.Name,
-		)
-
-		if err != nil {
-			custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
-			return []models.Language{}, errors.New(custom_errors.LanguageParsingError)
-		}
-
-		languageList = append(languageList, languageObj)
-	}
-
-	err = rows.Close()
-
-	if err != nil {
-		custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
-		return []models.Language{}, errors.New(custom_errors.LanguageClosingTableError)
-	}
-
-	return languageList, nil
-}
---
+++ server-api/repositories/language_repository.go
@@ -0,0 +1,66 @@
+package repositories
+
+import (
+	custom_errors "ai-fact-checker/server-api/errors"
+	"ai-fact-checker/server-api/models"
+	"database/sql"
+	"errors"
+)
+
+type LanguageRepository struct {
+	connection *sql.DB
+}
+
+// Create ----------------------------------------------------------------------------------
+
+func (lr *LanguageRepository) CreateLanguage(newsOutlet models.NewsOutlet) (int, error) {
+	_, err := lr.connection.Prepare("INSERT INTO languages (name) VALUES ($1) RETURNING id")
+	
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return -1, errors.New(custom_errors.LanguageNotFound)
+		}
+		// TODO implement missing handler for the scenario where the language already exists
+		return -1, err
+	}
+	
+	return -1, nil
+}
+
+// Read ------------------------------------------------------------------------------------
+
+func (lr *LanguageRepository) GetLanguages() ([]models.Language, error) {
+	query := "SELECT * FROM languages"
+	rows, err := lr.connection.Query(query)
+
+	if err != nil {
+		custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
+		return []models.Language{}, errors.New(custom_errors.LanguageTableMissing)
+	}
+
+	var languageList []models.Language
+	var languageObj models.Language
+
+	for rows.Next() {
+		err = rows.Scan(
+			&languageObj.Id,
+			&languageObj.Name,
+		)
+
+		if err != nil {
+			custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
+			return []models.Language{}, errors.New(custom_errors.LanguageParsingError)
+		}
+
+		languageList = append(languageList, languageObj)
+	}
+
+	err = rows.Close()
+
+	if err != nil {
+		custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
+		return []models.Language{}, errors.New(custom_errors.LanguageClosingTableError)
+	}
+
+	return languageList, nil
+}
