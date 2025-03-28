e99b9f1d077d3ca06238866057a6000d451a4a55
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:38:11 -0300

    implemented a brief main.go file to test the GUI

---
+++ client/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fact-ckert-client/gui"
+	"fact-ckert-client/models"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	config := models.NewConfig(false, true, true)
+	app := app.New()
+
+	gui.Build(app, config)
+}
