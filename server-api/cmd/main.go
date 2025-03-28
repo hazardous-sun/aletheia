0a7a3401b8461b7541eb1275bae7294ac81488d7
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:34:31 -0300

    added some comments and renamed 'db' variable to 'dbConnection'

--- server-api/cmd/main.go
+++ server-api/cmd/main.go
@@ -12,41 +12,42 @@ import (
 ) 
 
 func main() {
-    // Initialize the Database server
-    db, err := db.ConnectDB()
+	// Initialize the Database server
+	dbConnection, err := db.ConnectDB()
 
     if err != nil {
         custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
         return
     }
 
-    // Initializing the repository layer
-    languageRepository := repositories.NewLanguageRepository(db)
+	// Initializing the repository layer
+	languageRepository := repositories.NewLanguageRepository(dbConnection)
 
-    // Initializing the use case layer
-    languageUsecase := usecases.NewLanguageUsecase(languageRepository)
+	// Initializing the use case layer
+	languageUsecase := usecases.NewLanguageUsecase(languageRepository)
 
-    // Initializing the controller layer
-    languageController := controllers.NewLanguageController(languageUsecase)
+	// Initializing the controller layer
+	languageController := controllers.NewLanguageController(languageUsecase)
 
     // Initialize the API server
     server := gin.Default()
 
-    // Setting up HTTP paths in the API server--------------------------------
-
-    server.GET("/ping", func(ctx *gin.Context) {
-        ctx.JSON(200, gin.H{
-            "message": "pong",
-            "status": http.StatusOK,
-        })
-    })
-
-    server.GET("languages", languageController.GetLanguages)
-    server.GET("language/:languageId", languageController.GetLanguageById)
-
-    // ------------------------------------------------------------------------
-    
-    err = server.Run(":8000")
+    // Setting up HTTP paths in the API server--------------------------------------------------------------------------
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+			"status":  http.StatusOK,
+		})
+	})
+	// ----- Languages
+	// ---------- Create
+	server.POST("language", languageController.AddLanguage)
+	// ---------- Read
+	server.GET("languages", languageController.GetLanguages)
+	server.GET("language/:languageId", languageController.GetLanguageById)
+	// -----
+	// ----- News Outlets
+	// -----------------------------------------------------------------------------------------------------------------
 
     if err != nil {
         custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
