07cc7e46f1094aac1aaaedf2354ba2a256bcda03
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:29:54 -0300

    added go.mod and go.sum

--- server-api/cmd/main.go
+++ server-api/cmd/main.go
@@ -1,8 +1,26 @@
 package main
 
+ import ( 
   "github.com/gin-gonic/gin"
+ ) 
 
 func main() {
+    // Initialize the server
     server := gin.Default()
-}
 
+    // Setting up HTTP calls handling
+
+    server.GET("/ping", func(ctx *gin.Context) {
+        ctx.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+
+    // ------------------------------------------------------------------------
+    
+    err := server.Run(":8000")
+
+    if err != nil {
+        panic(err)
+    }
+}
