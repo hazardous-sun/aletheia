77823a9b0e2cb758c7b3b85895e80e88475c3b91
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:29:55 -0300

    added the 'language' and 'languages' path to the API

--- server-api/cmd/main.go
+++ server-api/cmd/main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"ai-fact-checker/server-api/controller"
+	"ai-fact-checker/server-api/controllers"
+	"ai-fact-checker/server-api/db"
+	custom_errors "ai-fact-checker/server-api/errors"
+	"ai-fact-checker/server-api/repositories"
+	"ai-fact-checker/server-api/usecases"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 ) 
 
 func main() {
-    // Initialize the server
+    // Initialize the Database server
+    db, err := db.ConnectDB()
+
+    if err != nil {
+        custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
+        return
+    }
+
+    // Initializing the repository layer
+    languageRepository := repositories.NewLanguageRepository(db)
+
+    // Initializing the use case layer
+    languageUsecase := usecases.NewLanguageUsecase(languageRepository)
+
+    // Initializing the controller layer
+    languageController := controllers.NewLanguageController(languageUsecase)
+
+    // Initialize the API server
     server := gin.Default()
 
-    // Setting up HTTP calls handling
+    // Setting up HTTP paths in the API server--------------------------------
 
     server.GET("/ping", func(ctx *gin.Context) {
         ctx.JSON(200, gin.H{
@@ -19,11 +42,15 @@ func main() {
         })
     })
 
+    server.GET("languages", languageController.GetLanguages)
+    server.GET("language/:languageId", languageController.GetLanguageById)
+
     // ------------------------------------------------------------------------
     
-    err := server.Run(":8000")
+    err = server.Run(":8000")
 
     if err != nil {
-        panic(err)
+        custom_errors.CustomLog(err.Error(), custom_errors.ErrorLevel)
+        return
     }
 }
