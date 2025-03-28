b154e1e18def5ee59a4696436eacaa05a7275ebc
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:35:16 -0300

    improved the run-server.sh script by adding some extra options for overwritting environment variables and defining if the pgdata/ directory should be removed during the service initialization

--- server-api/run-server.sh
+++ server-api/run-server.sh
@@ -7,6 +7,60 @@ export DB_USER="postgres"
 export DB_PASSWORD="1234"
 export DB_NAME="postgres"
 
+# Parse command-line options
+while getopts ":d" opt; do
+  case $opt in
+    d)
+      # If -d is passed, execute the command
+      echo "Running 'rm pgdata -r'..."
+      sudo rm -r pgdata
+      ;;
+    \?)
+      # Handle invalid options
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Shift the parsed options away, leaving only positional arguments
+shift $((OPTIND - 1))
+
+# Overwrite default values for the environment variables
+for arg in "$@"; do
+  case "$arg" in
+    --DB_HOST=*)
+      VALUE="${arg#*=}"
+      export DB_HOST="$VALUE"
+      echo "DB_HOST value overwritten"
+      ;;
+    --DB_PORT=*)
+      VALUE="${arg#*=}"
+      export DB_PORT="$VALUE"
+      echo "DB_PORT value overwritten"
+      ;;
+    --DB_USER=*)
+      VALUE="${arg#*=}"
+      export DB_USER="$VALUE"
+      echo "DB_USER value overwritten"
+      ;;
+    --DB_PASSWORD=*)
+      VALUE="${arg#*=}"
+      export DB_PASSWORD="$VALUE"
+      echo "DB_PASSWORD value overwritten"
+      ;;
+    --DB_NAME=*)
+      VALUE="${arg#*=}"
+      export DB_NAME="$VALUE"
+      echo "DB_NAME value overwritten"
+      ;;
+    *)
+      # Handle unknown arguments
+      echo "Unknown argument: $arg" >&2
+      ;;
+  esac
+done
+
 # Cleanup previously created containers of this project
 podman-compose down
 
