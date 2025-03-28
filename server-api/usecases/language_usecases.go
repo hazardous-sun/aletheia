c5fd191d4090397e14e8d83163c0872c9fac9315
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:29:55 -0300

    added some documentation to use cases

--- server-api/usecases/language_usecases.go
+++ server-api/usecases/language_usecases.go
@@ -17,10 +17,22 @@ func NewLanguageUsecase(repo *repositories.LanguageRepository) LanguageUseCase {
 
 // Read ------------------------------------------------------------------------------------------------------------------------------------
 
+// GetLanguages :
+// Returns all the languages stored in the database. Even though it may fail, it should not crash the application at any given moment.
+//
+// Error: will throw LanguageTableMissing if the database is incorrectly set and the "languages" table is missing.
+//
+// Error: will throw LanguageParsingError if for some reason it is unable to parse the values it receives from the database.
+//
+// Error: will throw LanguageClosingTableError if it fails to close the database rows.
 func (lu *LanguageUseCase) GetLanguages() ([]models.Language, error) {
     return lu.languageRepository.GetLanguages()
 }
 
+// GetLanguageById :
+// Returns a "language" instance by id. Even though it may fail, it should not crash the application at any given moment.
+//
+// Error: will throw LanguageNotFound if a language with the provided id is not found.
 func (lu *LanguageUseCase) GetLanguageById(id int) (*models.Language, error) {
     language, err := lu.languageRepository.GetLanguageById(id)
 
