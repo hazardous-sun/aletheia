06748a043f8378a38dc2ddc10ac37359057e174c
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-03-28 15:29:55 -0300

    Fixed missing package references inside databaes handlers

--- server-api/repositories/database_repository.go
+++ server-api/repositories/database_repository.go
@@ -1,10 +1,7 @@
 package repositories
 
 import (
-	custom_errors "ai-fact-checker/server-api/errors"
-	"ai-fact-checker/server-api/models"
 	"database/sql"
-	"errors"
 )
 
 type databaseRepository struct {
