43374504990a71d80b8e983207134a992cc9630b
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-04-05 18:46:27 -0300

    refactor: refactored the Config struct and added a Port field to it

--- client/src/models/config.go
+++ client/src/models/config.go
@@ -1,40 +1,88 @@
 package models
 
 import (
-	"fmt"
+	"errors"
+	client_errors "fact-ckert-client/src/errors"
 	"os"
+	"strconv"
 )
 
 type Config struct {
-	Image   bool
-	Video   bool
-	Context bool
+	Port    string `json:"port"`
+	Image   bool   `json:"image"`
+	Video   bool   `json:"video"`
+	Context bool   `json:"context"`
 }
 
-func NewConfig(image, video, context bool) *Config {
-	return &Config{image, video, context}
-}
+const (
+	Port    = "PORT"
+	Context = "CONTEXT"
+	Image   = "IMAGE"
+	Video   = "VIDEO"
+)
 
-func CollectConfigValues() ([]string, Config) {
-	var result []string
-	config := Config{false, false, false}
+// NewConfig :
+// Returns an instance of a Config struct, used to configure the GUI for the client application.
+// Will fail if the "PORT" environment variable is not initialized as a valid integer.
+func NewConfig() (Config, error) {
+	config := Config{"", false, false, false}
+	// Get the values for each field from the environment variables
+	err := getValue(&config, Port)
 
-	for _, v := range os.Environ() {
-		switch v {
-		case "CONTEXT=1":
-			config.Context = true
-			result = append(result, "CONTEXT")
-		case "IMAGE=1":
-			config.Image = true
-			result = append(result, "IMAGE")
-		case "VIDEO=1":
-			fmt.Println(v)
-			config.Video = true
-			result = append(result, "VIDEO")
-		default:
-			continue
-		}
+	if err != nil {
+		client_errors.Log(err.Error(), client_errors.ErrorLevel)
+		return Config{}, err
 	}
 
-	return result, config
+	_ = getValue(&config, Context)
+	_ = getValue(&config, Image)
+	_ = getValue(&config, Video)
+
+	// Check if the Port field was initialized
+	if config.Port == "" {
+		err := errors.New(client_errors.UninitializedPort)
+		client_errors.Log(client_errors.UninitializedPort, client_errors.WarningLevel)
+		return Config{}, err
+	}
+
+	warnMissingFields(config)
+
+	return config, nil
+}
+
+func getValue(config *Config, field string) error {
+	value, exists := os.LookupEnv(field)
+	switch field {
+	case Port:
+		// Check if the port is a valid integer
+		_, err := strconv.Atoi(config.Port)
+
+		if err != nil {
+			err := errors.New(client_errors.InvalidPortValue)
+			return err
+		}
+
+		config.Port = value
+	case Context:
+		config.Context = exists
+	case Image:
+		config.Image = exists
+	case Video:
+		config.Video = exists
+	}
+
+	return nil
+}
+
+// Warn if there are fields that will not be displayed
+func warnMissingFields(config Config) {
+	if !config.Context {
+		client_errors.Log(client_errors.UninitializedContext, client_errors.WarningLevel)
+	}
+	if !config.Image {
+		client_errors.Log(client_errors.UninitializedImage, client_errors.WarningLevel)
+	}
+	if !config.Video {
+		client_errors.Log(client_errors.UninitializedVideo, client_errors.WarningLevel)
+	}
 }
