1356ecd4fe234daac7e823ca086b4a6ca33588b0
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-04-06 10:30:54 -0300

    refactor: adjusted the color for info messages to cyan

--- client/src/errors/logs.go
+++ client/src/errors/logs.go
@@ -6,21 +6,22 @@ import (
 )
 
 const (
+	ErrorLevel   = "error"
 	InfoLevel    = "info"
 	WarningLevel = "warning"
-	ErrorLevel   = "error"
 )
 
 const (
 	defaultColor = "\033[0m"
 	errorColor   = "\033[91m"
+	infoColor    = "\033[0;36m"
 	warningColor = "\033[93m"
 )
 
 func Log(message string, level string) {
 	switch level {
 	case InfoLevel:
-		log.Println(fmt.Sprintf("%sinfo: %s %s", defaultColor, message, defaultColor))
+		log.Println(fmt.Sprintf("%sinfo: %s %s", infoColor, message, defaultColor))
 	case WarningLevel:
 		log.Println(fmt.Sprintf("%swarning: %s %s", warningColor, message, defaultColor))
 	case ErrorLevel:
