57b5c31770202b9b47197469db527d210e0cf727
solaire <141582502+hazardous-sun@users.noreply.github.com>
2025-04-13 16:10:11 -0300

    feat: added some error messages for the QueryParser

---
+++ server-api/src/errors/query_parser.go
@@ -0,0 +1,7 @@
+package server_errors
+
+const (
+	EmptyNewsOutletName = "EmptyNewsOutletName"
+	EmptyQueryParam     = "query parameters cannot be empty"
+	EmptyQueryUrl       = "query url cannot be empty"
+)
